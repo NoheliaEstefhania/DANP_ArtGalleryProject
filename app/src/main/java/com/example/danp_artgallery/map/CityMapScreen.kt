a3f38476a30118000e60ab4d4686f0e54938c9ff
DarwinJesusNeiraC <dneirac@unsa.edu.pe>
2024-06-17 19:04:21 -0500

    models of map for google and art gallery

--- app/src/main/java/com/example/danp_artgallery/map/CityMapScreen.kt
+++ app/src/main/java/com/example/danp_artgallery/map/CityMapScreen.kt
@@ -1,8 +1,6 @@
 package com.example.danp_artgallery.map
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 //import androidx.compose.foundation.layout.ColumnScopeInstance.align
@@ -10,8 +8,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.unit.sp
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.foundation.layout.fillMaxSize
@@ -19,20 +15,17 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.danp_artgallery.R
-import com.example.danp_artgallery.screens.section.BeaconsDetailFunction
-import com.example.danp_artgallery.screens.section.beaconList
 
 
-val title = "SELECT THE ART GALLERY"
+private val title = "SELECT THE ART GALLERY"
 @Composable
-fun MapScreen(){
+fun CityMapScreen(){
     Scaffold(
         topBar = {
             Column(
@@ -98,6 +91,6 @@ fun MapScreen(){
 
 @Preview
 @Composable
-fun MapPreview(){
-    MapScreen()
+fun CityMapPreview(){
+    CityMapScreen()
 }
