36d1fe9b032aefc1959479faad2c1a29b73d2a40
netaap <ntacca@unsa.edu.pe>
2024-06-17 20:54:48 -0500

    Exposition.kt for exposition details

--- app/src/main/java/com/example/danp_artgallery/model/Exposition.kt
+++ app/src/main/java/com/example/danp_artgallery/model/Exposition.kt
@@ -1,2 +1,10 @@
 package com.example.danp_artgallery.model
 
+import androidx.annotation.DrawableRes
+import java.io.Serializable
+
+data class Exposition(
+    @DrawableRes val imageResource:Int,
+    val title: String,
+    val expositions: List<String>
+): Serializable
