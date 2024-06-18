aa091d4a29ef1f1203d6909c7614d8756417d57b
netaap <ntacca@unsa.edu.pe>
2024-06-17 20:53:46 -0500

    DataProvider.kt file to store data

---
+++ app/src/main/java/com/example/danp_artgallery/model/DataProvider.kt
@@ -0,0 +1,44 @@
+package com.example.danp_artgallery.model
+
+import com.example.danp_artgallery.R
+import com.example.danp_artgallery.screens.section.Exposition
+
+object DataProvider {
+
+    val exposition = Exposition(
+        imageResource = R.drawable.ccunsa,
+        title = "CARPINTERO DE NIDOS\n",
+        expositions = listOf(
+            "De los hijos que se van. Daniel Gallegos. Daniel Gallegos. Centro Cultural UNSA. Galería I.Centro Cultural UNSA.\n",
+            "Daniel Gallegos\n"
+        )
+    )
+
+    val expositionList = listOf(
+        exposition,
+        Exposition(
+            imageResource = R.drawable.ccunsa,
+            title = "CARPINTERO DE NIDOS\n",
+            expositions = listOf(
+                "De los hijos que se van. Daniel Gallegos. Daniel Gallegos. Centro Cultural UNSA. Galería I.Centro Cultural UNSA.\n",
+                "Daniel Gallegos\n"
+            )
+        ),
+        Exposition(
+            imageResource = R.drawable.ccunsa,
+            title = "CARPINTERO DE NIDOS\n",
+            expositions = listOf(
+                "De los hijos que se van. Daniel Gallegos. Daniel Gallegos. Centro Cultural UNSA. Galería I.Centro Cultural UNSA.\n",
+                "Daniel Gallegos\n"
+            )
+        ),
+        Exposition(
+            imageResource = R.drawable.ccunsa,
+            title = "CARPINTERO DE NIDOS\n",
+            expositions = listOf(
+                "De los hijos que se van. Daniel Gallegos. Daniel Gallegos. Centro Cultural UNSA. Galería I.Centro Cultural UNSA.\n",
+                "Daniel Gallegos\n"
+            )
+        )
+    )
+}
