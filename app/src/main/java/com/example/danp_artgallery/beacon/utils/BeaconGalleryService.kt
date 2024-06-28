d9ad185928a79d8b48aaf0d32c3c46820c433ea4
SamirDiegoChavezCaceres <schavezca@unsa.edu.pe>
2024-06-27 19:18:27 -0500

    service beacon

---
+++ app/src/main/java/com/example/danp_artgallery/beacon/utils/BeaconGalleryService.kt
@@ -0,0 +1,181 @@
+package com.example.danp_artgallery.beacon.utils
+
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.Service
+import android.app.TaskStackBuilder
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.os.IBinder
+import android.util.Log
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+import androidx.lifecycle.Observer
+import com.example.danp_artgallery.MainActivity
+import com.example.danp_artgallery.R
+import org.altbeacon.beacon.Beacon
+import org.altbeacon.beacon.BeaconManager
+import org.altbeacon.beacon.BeaconParser
+import org.altbeacon.beacon.MonitorNotifier
+import org.altbeacon.beacon.Region
+
+class BeaconGalleryService : Service() {
+
+    override fun onBind(intent: Intent): IBinder? {
+        return null
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        val beaconManager = BeaconManager.getInstanceForApplication(this)
+        BeaconManager.setDebug(true)
+        val parser = BeaconParser().
+        setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24")
+        parser.setHardwareAssistManufacturerCodes(arrayOf(0x004c).toIntArray())
+        beaconManager.getBeaconParsers().add(
+            parser)
+        setupBeaconScanning()
+
+        return START_STICKY
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        val beaconManager = BeaconManager.getInstanceForApplication(this)
+        beaconManager.stopMonitoring(region)
+        beaconManager.stopRangingBeacons(region)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun setupBeaconScanning() {
+        val beaconManager = BeaconManager.getInstanceForApplication(this)
+
+        // By default, the library will scan in the background every 5 minutes on Android 4-7,
+        // which will be limited to scan jobs scheduled every ~15 minutes on Android 8+
+        // If you want more frequent scanning (requires a foreground service on Android 8+),
+        // configure that here.
+        // If you want to continuously range beacons in the background more often than every 15 mintues,
+        // you can use the library's built-in foreground service to unlock this behavior on Android
+        // 8+.   the method below shows how you set that up.
+        try {
+            setupForegroundService()
+        }
+        catch (e: SecurityException) {
+            // On Android TIRAMUSU + this security exception will happen
+            // if location permission has not been granted when we start
+            // a foreground service.  In this case, wait to set this up
+            // until after that permission is granted
+            Log.d(
+                TAG,
+                "Not setting up foreground service scanning " +
+                        "until location permission granted by user"
+            )
+            return
+        }
+        //beaconManager.setEnableScheduledScanJobs(false);
+        //beaconManager.setBackgroundBetweenScanPeriod(0);
+        //beaconManager.setBackgroundScanPeriod(1100);
+
+        // Ranging callbacks will drop out if no beacons are detected
+        // Monitoring callbacks will be delayed by up to 25 minutes on region exit
+        // beaconManager.setIntentScanningStrategyEnabled(true)
+
+        // The code below will start "monitoring" for beacons matching the region definition at the top of this file
+        beaconManager.startMonitoring(region)
+        beaconManager.startRangingBeacons(region)
+        // These two lines set up a Live Data observer so this Activity can get beacon data from the Application class
+        val regionViewModel = BeaconManager.getInstanceForApplication(
+            this
+        ).getRegionViewModel(region)
+        // observer will be called each time the monitored regionState changes (inside vs. outside region)
+        regionViewModel.regionState.observeForever( centralMonitoringObserver)
+        // observer will be called each time a new list of beacons is ranged (typically ~1 second in the foreground)
+        regionViewModel.rangedBeacons.observeForever( centralRangingObserver)
+
+    }
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun setupForegroundService() {
+        val builder = Notification.Builder(this, "BeaconReferenceApp")
+        builder.setSmallIcon(R.drawable.ic_launcher_background)
+        builder.setContentTitle("Scanning for Beacons")
+        val intent = Intent(this, MainActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(
+            this,
+            0,
+            intent,
+            PendingIntent.FLAG_UPDATE_CURRENT + PendingIntent.FLAG_IMMUTABLE
+        )
+        builder.setContentIntent(pendingIntent)
+        val channel =  NotificationChannel("beacon-ref-notification-id",
+            "My Notification Name", NotificationManager.IMPORTANCE_DEFAULT)
+        channel.setDescription("My Notification Channel Description")
+        val notificationManager =  getSystemService(
+            Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
+        builder.setChannelId(channel.getId())
+        Log.d(TAG, "Calling enableForegroundServiceScanning")
+        BeaconManager.getInstanceForApplication(this).enableForegroundServiceScanning(
+            builder.build(),
+            456
+        )
+        Log.d(TAG, "Back from  enableForegroundServiceScanning")
+    }
+    @RequiresApi(Build.VERSION_CODES.O)
+    val centralMonitoringObserver = Observer<Int> { state ->
+        if (state == MonitorNotifier.OUTSIDE) {
+            Log.d(TAG, "outside beacon region: "+region)
+        }
+        else {
+            Log.d(TAG, "inside beacon region: "+region)
+            sendNotification()
+        }
+    }
+
+    val centralRangingObserver = Observer<Collection<Beacon>> { beacons ->
+        val rangeAgeMillis = System.currentTimeMillis() - (
+                beacons.firstOrNull()?.lastCycleDetectionTimestamp ?: 0
+        )
+        if (rangeAgeMillis < 10000) {
+            Log.d(MainActivity.TAG, "Ranged: ${beacons.count()} beacons")
+            for (beacon: Beacon in beacons) {
+                Log.d(TAG, "$beacon about ${beacon.distance} meters away")
+            }
+        }
+        else {
+            Log.d(
+                MainActivity.TAG,
+                "Ignoring stale ranged beacons from $rangeAgeMillis millis ago"
+            )
+        }
+    }
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun sendNotification() {
+        val builder = NotificationCompat.Builder(this, "beacon-ref-notification-id")
+            .setContentTitle("Beacon Reference Application")
+            .setContentText("A beacon is nearby.")
+            .setSmallIcon(R.drawable.ic_launcher_background)
+        val stackBuilder = TaskStackBuilder.create(this)
+        stackBuilder.addNextIntent(Intent(this, MainActivity::class.java))
+        val resultPendingIntent = stackBuilder.getPendingIntent(
+            0,
+            PendingIntent.FLAG_UPDATE_CURRENT + PendingIntent.FLAG_IMMUTABLE
+        )
+        builder.setContentIntent(resultPendingIntent)
+        val channel =  NotificationChannel("beacon-ref-notification-id",
+            "My Notification Name", NotificationManager.IMPORTANCE_DEFAULT)
+        channel.setDescription("My Notification Channel Description")
+        val notificationManager =  getSystemService(
+            Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
+        builder.setChannelId(channel.getId())
+        notificationManager.notify(1, builder.build())
+    }
+
+    companion object {
+        var region = Region("all-beacons", null, null, null)
+        val TAG = "BeaconService"
+    }
+}
