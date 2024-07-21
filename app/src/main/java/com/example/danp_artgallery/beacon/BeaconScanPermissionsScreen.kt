dabde9cf9f39ade848fe7ecb385200ff8ec986b6
netaap <ntacca@unsa.edu.pe>
2024-07-21 16:26:40 -0500

    topbar correction of beacon screem

--- app/src/main/java/com/example/danp_artgallery/beacon/BeaconScanPermissionsScreen.kt
+++ app/src/main/java/com/example/danp_artgallery/beacon/BeaconScanPermissionsScreen.kt
@@ -72,35 +72,10 @@ fun BeaconScanPermissionsScreen(navController: NavController) {
                         .padding(vertical = 8.dp),
                     verticalAlignment = Alignment.CenterVertically
                 ) {
-                    Image(
-                        painter = painterResource(id = R.drawable.arrow), // Reemplaza 'arrow' con el ID de tu imagen de flecha
-                        contentDescription = "flecha",
-                        modifier = Modifier
-                            .padding(start = 16.dp) // Añade padding al lado izquierdo
-                            .size(50.dp)
-                    )
 
-                    Spacer(modifier = Modifier.weight(1f)) // Espacio flexible para empujar el logo al centro
-
-                    Image(
-                        painter = painterResource(id = R.drawable.logo), // Reemplaza 'logo' con el ID de tu imagen
-                        contentDescription = "logo",
-                        modifier = Modifier
-                            .size(50.dp)
-                    )
-
-                    Spacer(modifier = Modifier.weight(1f)) // Espacio flexible para empujar el ícono de configuración a la derecha
-
-                    Image(
-                        painter = painterResource(id = R.drawable.settings_img), // Reemplaza 'settings_img' con el ID de tu ícono de configuración
-                        contentDescription = "configuración",
-                        modifier = Modifier
-                            .padding(end = 16.dp) // Añade padding al lado derecho
-                            .size(50.dp)
-                    )
                 }
 
-                Spacer(modifier = Modifier.height(16.dp)) // Espacio entre la fila de íconos y el texto, ajusta el valor según sea necesario
+                Spacer(modifier = Modifier.height(50.dp)) // Espacio entre la fila de íconos y el texto, ajusta el valor según sea necesario
 
             }
         },
