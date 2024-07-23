986a1b5a880f240d595cf026d222c8fec2a878c2
netaap <ntacca@unsa.edu.pe>
2024-07-22 20:21:30 -0500

    app colors - theme

--- app/src/main/java/com/example/danp_artgallery/MainActivity.kt
+++ app/src/main/java/com/example/danp_artgallery/MainActivity.kt
@@ -6,21 +6,26 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.RequiresApi
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import com.example.danp_artgallery.navigation.AppNavigation
+import com.example.danp_artgallery.ui.theme.DANP_ArtGalleryTheme
 
 class MainActivity : ComponentActivity() {
-//    lateinit var beaconReferenceApplication: BeaconReferenceApplication
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-//            beaconReferenceApplication = application as BeaconReferenceApplication
-
-            AppNavigation(this, this)
+            DANP_ArtGalleryTheme {
+                Surface(color = MaterialTheme.colorScheme.background) {
+                    AppNavigation(this, this)
+                }
+            }
         }
     }
+
     companion object {
         const val TAG = "MainActivity"
     }
